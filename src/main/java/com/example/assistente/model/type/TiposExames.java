c4774de3561ccc36077728dc9c27689c5cf8ea47
Lucas Costa de Jesus Santos <lucas.cos.je.sa@gmail.com>
2020-08-31 16:16:59 -0300

    Corrige e adiciona novas regras

--- src/main/java/com/example/assistente/model/type/TiposExames.java
+++ src/main/java/com/example/assistente/model/type/TiposExames.java
@@ -1,21 +1,21 @@
 package com.example.assistente.model.type;
 
 public enum TiposExames {
-	globulos_brancos,
-	globulos_vermelhos,
-	plaquetas,
+	globulos_brancos,//
+	globulos_vermelhos,//
+	plaquetas,//
 	ttpa_tp,
-	hdl,
+	hdl,//
 	ldl,
-	triglicerideos,
-	ureia_creatinina,
-	glicose,
+	triglicerideos,//
+	ureia_creatinina,//
+	glicose,//
 	tgo_tgp,
 	eletrolitos,
 	tsh_t4_livre,
-	acido_urico, 
-	pcr,
-	psa,
-	vhs,
-	albumina
+	acido_urico,// 
+	pcr,//
+	psa,//
+	vhs,//
+	albumina//
 }

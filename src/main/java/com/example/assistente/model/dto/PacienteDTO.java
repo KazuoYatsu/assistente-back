5234b29bced06d7949c05bc4d2d95fbb83817ae1
Lucas Costa de Jesus Santos <lucas.cos.je.sa@gmail.com>
2020-09-08 21:46:29 -0300

    correção

--- src/main/java/com/example/assistente/model/dto/PacienteDTO.java
+++ src/main/java/com/example/assistente/model/dto/PacienteDTO.java
@@ -1,15 +1,36 @@
 package com.example.assistente.model.dto;
 
+import java.util.Date;
+
 import com.example.assistente.model.type.SexoPaciente;
 
 public class PacienteDTO {
+	
+	private int id;	
 	private SexoPaciente sexo;
+	private Date dataNasc;
 	
 	public SexoPaciente getSexo() {
 		return sexo;
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public void setSexo(SexoPaciente sexo) {
 		this.sexo = sexo;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
 	}
 }

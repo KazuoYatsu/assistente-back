9e8f29034107556e851f28c6270ab2bf60a3cdf5
Lucas Costa de Jesus Santos <lucas.cos.je.sa@gmail.com>
2020-09-30 18:11:10 -0300

    acido urico

--- src/main/java/com/example/assistente/model/dto/ResultadoDTO.java
+++ src/main/java/com/example/assistente/model/dto/ResultadoDTO.java
@@ -1,5 +1,6 @@
 package com.example.assistente.model.dto;
 
+import com.example.assistente.model.type.SexoPaciente;
 import com.example.assistente.model.type.Status;
 import com.example.assistente.model.type.TiposExames;
 import lombok.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 public class ResultadoDTO {
     private TiposExames tipoExame;
+    private SexoPaciente sexo;
     private String mensagem;
     private List<Double> values;
     private Status status;
